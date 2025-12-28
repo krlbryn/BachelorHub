0e5fff1667efa6b442d94831707164baac040108
NajibRealMe <157209434+NajibRealMe@users.noreply.github.com>
2025-12-28 20:34:01 +0800

    first commit

---
+++ src/main/java/com/mycompany/electionmanagementsystem/ElectionManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electionmanagementsystem;
+
+/**
+ *
+ * @author ParaNon
+ */
+public class ElectionManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
