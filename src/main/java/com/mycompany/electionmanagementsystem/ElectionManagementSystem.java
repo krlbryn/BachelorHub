06aab8f4822405d3ea2ea8b78bf564db5ddb6aed
NajibRealMe <157209434+NajibRealMe@users.noreply.github.com>
2025-12-28 22:39:30 +0800

    first commit

--- src/main/java/com/mycompany/electionmanagementsystem/ElectionManagementSystem.java
+++ src/main/java/com/mycompany/electionmanagementsystem/ElectionManagementSystem.java
@@ -11,6 +11,6 @@ package com.mycompany.electionmanagementsystem;
 public class ElectionManagementSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello There!");
     }
 }
