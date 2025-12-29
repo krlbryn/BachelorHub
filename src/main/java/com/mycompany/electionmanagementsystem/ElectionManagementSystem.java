a5a2a3dedd41bbc31c2c01233f972bdce3577684
NajibRealMe <157209434+NajibRealMe@users.noreply.github.com>
2025-12-29 23:44:17 +0800

    fourth commit

--- src/main/java/com/mycompany/electionmanagementsystem/ElectionManagementSystem.java
+++ src/main/java/com/mycompany/electionmanagementsystem/ElectionManagementSystem.java
@@ -15,5 +15,6 @@ public class ElectionManagementSystem {
         System.out.println("Hello Here!");
         System.out.println("Hello There!");
         System.out.println("Najib second output command is here!");
+        System.out.println("Najib third output command is here!");
     }
 }
